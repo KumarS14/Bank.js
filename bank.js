b012712ad5cf5f3ec4dde5770b10a9bf96480227
KumarS14 <sonjaykumar1404@gmail.com>
2023-04-17 15:58:05 +0100

    date doesnt format properly in irb

--- bank.js
+++ bank.js
@@ -1,6 +1,6 @@
 class Bank {
     constructor(){
-       this.date = ['10/01/2023']
+       this.date = []
        this.current = [1000]
        this.amount = [1000]
     }
@@ -8,9 +8,9 @@ class Bank {
     {
       return`date||credit || debit || balance\n         ${this.date}     || ${this.amount}  || ${this.current}`
     }
-    deposit(insert)
+    deposit()
     {
-        
+        console.log('enter date followed by amount you want to deposit ')
     }
 }
 module.exports = Bank;
