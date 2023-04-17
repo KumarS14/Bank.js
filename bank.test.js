a0ab2922befdbb0c086536b632547a5b3915c037
KumarS14 <sonjaykumar1404@gmail.com>
2023-04-17 15:48:07 +0100

    changed to matched format

--- bank.test.js
+++ bank.test.js
@@ -10,6 +10,7 @@ describe('bank',() => {
     it('tests to see if balance returns zero',() =>
     {
        const bank1 = new bank()
-        expect(bank1.balance()).toEqual()
+        expect(bank1.balance()).toEqual(`date||credit || debit || balance
+         10/01/2023     || 1000  || 1000`)
     });
 });
