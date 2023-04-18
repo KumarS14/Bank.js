d2fd2d33e31e704cfb7bfa9c776c4e017ea44bca
KumarS14 <sonjaykumar1404@gmail.com>
2023-04-18 14:59:20 +0100

    added 2 variables and altered balance code

--- bank.js
+++ bank.js
@@ -1,15 +1,18 @@
 let readline = require('readline-sync')
 class Bank {
     constructor(){
-       this.date = ['10/01/2023','34/45/2045']
-       this.current = [1000,200]
-       this.amount = [1000,300]
+       this.date = []
+       this.current = []
+       this.depositOrWithdraw = []
+       this.bool = true
+       this.question = ""
     }
     balance()
     {
+        
         let output = '\t date || credit || debit ||  balance\n';
         for (let i = 0; i < this.date.length; i++) {
-         output += `          \t  ${this.date[ i]}   \t|| ${this.amount[i]}   || ${this.current[i]}\n`;
+         output += `          \t  ${this.date[ i]}   \t|| ${this.depositOrWithdraw[i]}   || ${this.newBalance[i]}\n`;
        
         }
         return output
@@ -19,8 +22,16 @@ class Bank {
     
     deposit()
     {
-       this.date.push(readline.question('enter date of your deposit'))
-
+        while (true) {
+       this.date.push(readline.question('enter date of your deposit : '))
+       this.depositOrWithdraw.push(readline.question('enter the amount you want to deposit in : '))
+       this.current.push(this.depositOrWithdraw)
+     
+     
+       this.question = (readline.question('would you like to create another transaction or if you are done please type end : ')) 
+       if (this.question == 'exit'){ break}
+       
+    }     
         
 
     }
