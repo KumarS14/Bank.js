4d76c63837c22f1e4674afe100b65ce2fe800a9f
KumarS14 <sonjaykumar1404@gmail.com>
2023-04-19 17:25:51 +0100

    testif module can return two transactions

--- bank.test.js
+++ bank.test.js
@@ -4,25 +4,46 @@ describe("Bank", () => {
 
   it('tests to see if file can be called',() => {
     bank1 = new Bank()
-    expect(bank1.balance()).toBe ( '     ||date || credit|| debit ||  balance\n')
+    expect(bank1.balance()).toBe( '||date || credit|| debit ||  balance')
 
   });
   it('tests to see if file can return one transaction',() => {
-    bank1 = new Bank()
-    bank1.transaction('13/04/2005','+1000','exit')
-    expect(bank1.balance()).toEqual(expect.stringContaining('||12/03/2004    ||+1000  || 1000'));
+    const bank1 = new Bank();
+    bank1.transaction('13/04/2005','+1000','no');
+    const expected = [
+      "||date || credit|| debit ||  balance",
+      "   ||13/04/2005    ||+1000  || 1000",
+     
+    ];
+    expect(bank1.balance().split('\n')).toEqual(expected);
+  
 
   });
   it('tests to see if file can return two transaction',() => {
-    bank1 = new Bank()
+  
+      const bank1 = new Bank();
+      bank1.transaction('13/04/2005','+1000','no');
+      bank1.transaction('14/05/2006','-50','exit');
+      const expected = [
+        "||date || credit|| debit ||  balance",
+        "   ||13/04/2005    ||+1000  || 1000",
+        "   ||14/05/2006    ||-50  || 950",
+      ];
+      expect(bank1.balance().split('\n')).toEqual(expected);
     
-      bank1.transaction('13/04/2005','+1000','no')
-      bank1.transaction('14/05/2006','-50','exit')
+    
+    
+  });
     
     
 
-    expect(bank1.balance()).toEqual("dfdf")
+
+
+   
+  
+ 
+
   });
  
-})  
+
 
