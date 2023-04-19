c68313147e82e445aa14ea4fe64b46827092c06a
KumarS14 <sonjaykumar1404@gmail.com>
2023-04-19 14:59:24 +0100

    tests if bank returns values in correct format

--- bank.test.js
+++ bank.test.js
@@ -1,10 +1,17 @@
 
 const Bank = require("./bank");
-
 describe("Bank", () => {
-  it('tests to see if module is called',() => {
+
+  it('tests to see if file can be called',() => {
     bank1 = new Bank()
     expect(bank1.balance()).toBe ( '     ||date || credit|| debit ||  balance\n')
+
+  })
+  it('tests to see if file can be called',() => {
+    bank1 = new Bank()
+    bank1.transaction('12/03/2004','+1000','exit')
+    expect(bank1.balance()).toBe['||date || credit|| debit ||  balance'
+    ,'||12/03/2004    ||+1000  || 1000']
 
   })
 })  
