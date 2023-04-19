6884c37518a103e201d7ba2791854e6aa55e18ca
KumarS14 <sonjaykumar1404@gmail.com>
2023-04-19 16:04:05 +0100

    test to see if 2 transactions can be taken in

--- bank.test.js
+++ bank.test.js
@@ -13,6 +13,16 @@ describe("Bank", () => {
     expect(bank1.balance()).toEqual(expect.stringContaining('||12/03/2004    ||+1000  || 1000'));
 
   });
+  it('tests to see if file can return two transaction',() => {
+    bank1 = new Bank()
+    
+      bank1.transaction('13/04/2005','+1000','no')
+      bank1.transaction('14/05/2006','-50','exit')
+    
+    
+
+    expect(bank1.balance()).toEqual("dfdf")
+  });
  
 })  
 
